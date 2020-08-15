0487b2b54147247a0c7ea9bd2966aedbe5f22c81
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2020-08-15 12:54:51 -0400

    Adding more code
    Learned that not all websites allow scraping - so it reduces amount of viable websites to include. Learning to use BS. Will need to find Viable site - I remember reddit had a image search feature

--- Source Files/Start_2.py
+++ Source Files/Start_2.py
@@ -1,12 +1,24 @@
-from bs4 import BeautifulSoup
-
-import requests
+import bs4 as bs
 import urllib.request
 import shutil
+from urllib.request import Request, urlopen
 
+search = input("Enter a search term\n")
 
-url = "https://wallpaperaccess.com/dragon-ball-goku"
-response = requests.get(url)
+req = Request("https://www.lifeofpix.com/search/{}?".format(search), headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
+
+webpage = urlopen(req).read()
+
+soup = bs.BeautifulSoup(webpage, "lxml")
+
+for x in soup.find_all("a"):
+    print(x.get("href"))
+
+#print(dir(soup))
+
+#print(soup)
+
+'''response = requests.get(url)
 
 soup = BeautifulSoup(response.text, "html.parser")
 
@@ -21,7 +33,7 @@ print(response)
 
 # ("./bin/")
 
-
+'''
 
 
 
