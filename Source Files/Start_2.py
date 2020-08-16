47be68ad9e91e9637015b0f359c64dbfd4739b40
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2020-08-16 11:37:40 -0400

    Downloading is working - buggy
    Downloading the .jpg links works, but currently makes duplicates of each image.

--- Source Files/Start_2.py
+++ Source Files/Start_2.py
@@ -2,13 +2,11 @@ import bs4 as bs
 import urllib.request
 import shutil
 from urllib.request import Request, urlopen
+import requests
 
 search = input("Enter a search term\n")
 
 req = Request("https://www.reddit.com/r/Images/search?q={}&restrict_sr=1".format(search), headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
-
-#req = Request("https://www.reddit.com/r/Images/search?q=ocean&restrict_sr=1", headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'})
-
 
 webpage = urlopen(req).read()
 
@@ -22,44 +20,24 @@ for x in soup.find_all("a"):
     raw_links.append(x.get("href"))
 
 
-
 for y in range(0, len(raw_links)):
     if raw_links[y].find(".jpg") > 0:
         jpg_links.append(raw_links[y])
 
+n = 1
+for y in range(0, len(jpg_links)):
+    dl = str(jpg_links[y])
+    r = requests.get(dl, allow_redirects=True)
+    open("./bin/image_{}.jpg".format(n), "wb").write(r.content)
+    n += 1
 
 
-print(raw_links)
-print(jpg_links)
+input("Program Complete - Press enter to close")
 
-#print(soup)
+#for n in range(1, len(jpg_links) + 1):
+#    print("image_{}".format(n))
 
-'''response = requests.get(url)
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-aas = soup.find_all("a")
-
-print(response)
-
-#imgurl = "https://www.google.com/" + aas[0]["src"]
-
-#req.urlretrieve(imgurl, "./bin/image.png")
+#print(len(jpg_links))
 
 
 # ("./bin/")
-
-'''
-
-
-
-
-
-
-
-
-
-
-
-
-input("Press Enter to close")
